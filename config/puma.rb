b1f080dfdad335e18c285744f86a9067041e1357
Rafael Vazquez <rafavg77@gmail.com>
2020-08-19 01:01:37 -0500

    Se cambia la pase de datos a mysql

--- config/puma.rb
+++ config/puma.rb
@@ -11,8 +11,7 @@ threads min_threads_count, max_threads_count
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
 port        ENV.fetch("PORT") { 3000 }
-bind	'tcp://10.0.2.15:3000'
-
+bind 'tcp://10.0.2.15:3000'
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_19_050024) do
+ActiveRecord::Schema.define(version: 2020_08_19_053339) do
 
-  create_table "todo_lists", force: :cascade do |t|
+  create_table "todo_items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "content"
+    t.bigint "todo_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["todo_list_id"], name: "index_todo_items_on_todo_list_id"
+  end
+
+  create_table "todo_lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "todo_items", "todo_lists"
 end
